d72e6b89801d8c67bb2147f0d941c2d25238dbc3
matt-komm <Matthias.Komm@cern.ch>
2022-02-04 12:55:56 +0000

    tests

---
+++ test/runCMSSWTest.sh
@@ -0,0 +1,25 @@
+function run_test()
+{
+    cd ~
+    source /cvmfs/cms.cern.ch/cmsset_default.sh
+    export SCRAM_ARCH=slc7_amd64_gcc820 || return 1
+    echo
+    echo "==================== setup CMSSW ==============="
+    scramv1 project CMSSW CMSSW_11_1_7 || return 1
+    cd CMSSW_11_1_7/src || return 1
+    eval `scram runtime -sh` || return 1
+    mkdir -p PhysicsTools/NanoAODTools
+    rsync -r --stats /scripts/ PhysicsTools/NanoAODTools/. || return 1
+    echo
+    echo "==================== compiling ================"
+    scram b || return 1
+    
+    echo
+    echo "==================== 2017 ==============="
+    python PhysicsTools/NanoAODTools/processors/ttX3.py --year 2017 --input=https://github.com/ttXcubed/test-files/raw/main/nanoAODv9UL/TTTo2L2Nu_HT500Njet7_TuneCP5_13TeV-powheg-pythia8_2017.root . || return 1
+
+    
+    echo "==================== done ====================="
+}
+
+run_test
