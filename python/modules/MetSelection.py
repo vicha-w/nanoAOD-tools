51c56b1180dd0149235cf7bc118c3f161e847d62
Vichayanun Wachirapusitanand <vichayanun.wachirapusitanand@cern.ch>
2023-11-29 02:23:20 +0700

    Fixed MetSelection again

--- python/modules/MetSelection.py
+++ python/modules/MetSelection.py
@@ -1,4 +1,4 @@
-from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import (Collection, Object)
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
