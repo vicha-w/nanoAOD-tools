bdb33dd5eec5ab35d6c75e6cdb1bc0fd96f0bfce
Vichayanun Wachirapusitanand <vichayanun.wachirapusitanand@cern.ch>
2024-04-03 17:31:21 +0700

    Enough! I don't want to rewrite this every time I pull on lxplus.

--- postprocessor/single_muon_batchprocessor.sh
+++ postprocessor/single_muon_batchprocessor.sh
@@ -1,6 +1,20 @@
-mkdir -p batchprocessor_output
-for d in $(ls ../HOTVR_nanoaod/2017_newcuts/*/ -d)
-do
-    echo "Processing" $(basename $d)
-    root -l "postprocessor/single_muon_postprocessor.cpp(\"$d/NANO*.root\", \"batchprocessor_output/$(basename $d).root\")" -q
-done
+#!/bin/bash
+export STARTDIR=/afs/cern.ch/user/v/vwachira/CMSSW_11_1_7/src/PhysicsTools/NanoAODTools
+cd /afs/cern.ch/user/v/vwachira/CMSSW_11_1_7/src
+eval `scramv1 runtime -sh`
+
+cd $STARTDIR
+
+INDIR=$1
+OUTDIR=/eos/home-v/vwachira/HOTVR_nanoaod_output/2017/batchprocessor_output/$2.root
+ISDATA=$3
+UNCMODE=$4
+
+echo "Processing" $INDIR
+echo "Writing inputs to" $OUTDIR
+echo "isData =" $ISDATA
+echo "uncmode =" $UNCMODE
+
+echo "Starting at" $(date)
+root -l "postprocessor/single_muon_postprocessor.cpp(\"${INDIR}/NANO*.root\", \"${OUTDIR}\", ${ISDATA}, ${UNCMODE})" -q
+echo "Finished at" $(date)
