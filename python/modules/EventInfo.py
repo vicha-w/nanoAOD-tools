e54be85477870ba0bf4085b223072648d3d51671
Vichayanun Wachirapusitanand <vichayanun.wachirapusitanand@cern.ch>
2025-06-20 15:14:28 +0700

    Updated EventInfo module

--- python/modules/EventInfo.py
+++ python/modules/EventInfo.py
@@ -4,17 +4,26 @@ import math
 import json
 import ROOT
 import random
+import yaml
+import re
 
-from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Event, InputTree
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
 class EventInfo(Module):
     def __init__(
         self,
-        storeVariables = []
+        storeVariables = [],
     ):
         self.storeVariables = storeVariables
-        
+
+        self.genEventSumw = 0
+        self.genEventCount = 0
+        self.genEventSumw2 = 0
+        self.LHEScaleSumw = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        self.genEventInfoInFile = False
+
     def beginJob(self):
         pass
     def endJob(self):
@@ -23,7 +32,12 @@ class EventInfo(Module):
         self.out = wrappedOutputTree
         for variable in self.storeVariables:
             variable[0](self.out)
-        
+
+        runsTree = InputTree(inputFile.Get("Runs"))
+        runsEvent = Event(runsTree, 0)
+        if hasattr(runsEvent, "genEventSumw"):
+            self.genEventInfoInFile = True
+
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
 
         if not Module.globalOptions["isData"]:            
@@ -34,13 +48,13 @@ class EventInfo(Module):
                 self.genEventCount = runsEvent.genEventCount
                 self.genEventSumw2 = runsEvent.genEventSumw2
                 self.LHEScaleSumw = [runsEvent.LHEScaleSumw[i] for i in range(9)]
-
             nGenEventCount_parameter = ROOT.TParameter(float)("genEventCount", self.genEventCount)
             nGenWeight_parameter = ROOT.TParameter(float)("sumGenWeights", self.genEventSumw)
             genEventSumw2_parameter = ROOT.TParameter(float)("sumGenWeights2", self.genEventSumw2)
             LHEScaleSumw_parameter = []
             # -- in case the parameters are not already stored in "Runs" TTree of the input file,
             # -- a manual calculation is required --> https://cms-nanoaod-integration.web.cern.ch/autoDoc/NanoAODv9/2017UL/doc_TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1.html
+
 
             if self.genEventInfoInFile == True:
                 for i in range(9): LHEScaleSumw_parameter.append(ROOT.TParameter(float)("LHEScaleSumw_{}".format(i), self.LHEScaleSumw[i]))
@@ -56,7 +70,16 @@ class EventInfo(Module):
             pass
         
     def analyze(self, event):
+
+        if not Module.globalOptions["isData"]:
+            # -- in case the parameters are not already stored in "Runs" TTree of the input file,
+            # -- a manual calculation is required
+            if self.genEventInfoInFile == False:
+                self.genEventSumw += event.Generator_weight
+                self.genEventCount += 1
+                self.genEventSumw2 += event.Generator_weight ** 2
+                for i in range(9): self.LHEScaleSumw[i] += event.LHEScaleWeight[i] * event.Generator_weight
+
         for variable in self.storeVariables:
-            variable[1](self.out,event)
+            variable[1](self.out, event)
         return True
-        
